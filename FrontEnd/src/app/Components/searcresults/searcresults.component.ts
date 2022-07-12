2e6f6eef99484e66dbc920dc9335a6f2065166aa
KristianBarrera <201720054@tese.edu.mx>
2022-07-12 01:54:27 -0500

    Search-bar

---
+++ FrontEnd/src/app/Components/searcresults/searcresults.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searcresults',
+  templateUrl: './searcresults.component.html',
+  styleUrls: ['./searcresults.component.css']
+})
+export class SearcresultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
